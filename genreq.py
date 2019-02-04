c757d4fdcc4179c222419e410376fc85a8f3b001
Jan Hohmann <github@jnhmn.de>
2019-02-04 17:52:28 +0100

    Refactoring: Request Extensions

--- genreq.py
+++ genreq.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 from cryptography import x509
 from cryptography.x509.oid import NameOID
+from cryptography.x509.oid import ExtendedKeyUsageOID
 from cryptography.hazmat.primitives import hashes
 
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -32,6 +33,33 @@ def gen_alt_name(list):
 
     alt_name_objs.append(x509.DNSName(name))
   return x509.SubjectAlternativeName(alt_name_objs)
+
+def gen_basic_key_usage():
+  return x509.KeyUsage(
+    content_commitment=False,
+    digital_signature=True,
+    key_encipherment=True,
+    data_encipherment=False,
+    key_agreement=False,
+    key_cert_sign=False,
+    crl_sign=False,
+    decipher_only=False,
+    encipher_only=False
+  )
+
+def gen_extended_key_usage():
+  usages = []
+  usages.append(ExtendedKeyUsageOID.SERVER_AUTH)
+  return x509.ExtendedKeyUsage(usages)
+
+def gen_csr(key, subject, extensions):
+  builder = x509.CertificateSigningRequestBuilder()
+  builder = builder.subject_name(x509.Name(subject))
+  for ext in extensions:
+    builder = builder.add_extension(ext[0], critical=ext[1])
+  # Sign the CSR with our private key.
+  csr = builder.sign(key, hashes.SHA256(), default_backend())
+  return csr
 
 config = configparser.RawConfigParser()
 config.read('config.ini')
@@ -92,14 +120,13 @@ while True:
   else:
     alt_names.add(name)
 
-key = rsa.generate_private_key(public_exponent=65537,key_size=4096,backend=default_backend());
+key = rsa.generate_private_key(public_exponent=65537,key_size=4096,backend=default_backend())
+ext_list = []
+ext_list.append((gen_alt_name(alt_names), False))
+ext_list.append((gen_basic_key_usage(), False))
+ext_list.append((gen_extended_key_usage(), False))
 
-csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name(subject)).add_extension(gen_alt_name(alt_names),
-    critical=False,
-).add_extension(
-    x509.KeyUsage(content_commitment=True,digital_signature=True,key_encipherment=True,data_encipherment=False,key_agreement=True,key_cert_sign=False,crl_sign=False,decipher_only=False, encipher_only=False),critical=False
-# Sign the CSR with our private key.
-).sign(key, hashes.SHA256(), default_backend())
+csr = gen_csr(key,subject,ext_list)
 
 with open(cn + ".key", "wb") as f:
     f.write(key.private_bytes(
