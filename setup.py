5a6076c787b3617655fd549638f60ea1a0e894b9
Heiko Carrasco <Miterion@users.noreply.github.com>
2019-02-16 13:29:19 +0100

    Fixed repo url in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
         description = 'Create CSR on the command line',
         install_requires = ['cryptography'],
         packages=find_packages(),
-        url='https://github.com/jnhmn/python-simple-ca',
+        url='https://github.com/jnhmn/python_simple_ca',
         entry_points={
             'console_scripts': [
                 'genreq=python_simple_ca.genreq:console_entry',
